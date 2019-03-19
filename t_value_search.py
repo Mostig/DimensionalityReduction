e9811de1be0a16b9d215c57a5b186a210c74ec78
Patrycja Jakimów <patrycja.jakimow@gmail.com>
2019-03-19 13:02:37 +0100

    add script for searching t-constant in LaplacianScore

---
+++ t_value_search.py
@@ -0,0 +1,27 @@
+from LaplacianScore import LaplacianScore
+import numpy as np
+import pandas as pd
+
+attributes = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width']
+class_label = ['class']
+data = pd.read_table('data/iris.data', sep=',', names=attributes+class_label)
+
+print("-------------Laplacian Score--------------------")
+print("-------------t value search---------------------")
+
+results = []
+correct_values = []
+
+t_values = np.arange(-100, 100, 1)
+for t in t_values:
+    print('---test value: ', t, '---')
+    l = LaplacianScore(k_nearest_neighbors=t, constant_t=0.00001, denominator_value=0.0001)
+    l.fit(data.iloc[:, 0:4])
+    results += l.feature_ranking_tuples
+    if  l.features_ordered[0:2] == [2, 3] or  l.features_ordered[0:2] == [3, 2]:
+        correct_values += t
+        print('found!')
+        print('\tresult: ', l.features_ordered)
+    
+        
+        
